88c00b821f1c5a3f1cb75557a07a3ce9eaa1976b
Alison Soares <alisonfsoares@gmail.com>
2017-07-05 22:34:30 -0300

    First Step
    Create classes / default pages base.

---
+++ Bibliotech/src/java/modelo/AutorFacade.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author Alison
+ */
+@Stateless
+public class AutorFacade extends AbstractFacade<Autor> {
+    @PersistenceContext(unitName = "BibliotechPU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public AutorFacade() {
+        super(Autor.class);
+    }
+    
+}
